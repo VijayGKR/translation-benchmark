4de7bac6fccfbe061e2af58b5a47551f5772a8ad
David Bai <davidbai2020@gmail.com>
2024-09-17 00:25:08 -0700

    google translate benchmark

--- closed_source_eval.py
+++ closed_source_eval.py
@@ -1,6 +1,7 @@
 import deepl
 import os
 import dotenv
+from google.cloud import translate_v2 as translate
 dotenv.load_dotenv()
 
 class DeepL():
@@ -45,10 +46,47 @@ class DeepL():
     
 class GoogleTranslate():
     def __init__(self):
-      self
+        self.client = translate.Client()
+
+        self.target_langs = [
+        'AR',
+        'BG',
+        'CS',
+        'DA',
+        'DE',
+        'EL',
+        'EN-US',
+        'ES',
+        'ET',
+        'FI',
+        'FR',
+        'HU',
+        'ID',
+        'IT',
+        'JA',
+        'KO',
+        'LT',
+        'LV',
+        'NB',
+        'NL',
+        'PL',
+        'PT-PT',
+        'RO',
+        'RU',
+        'SK',
+        'SL',
+        'SV',
+        'TR',
+        'UK',
+        'ZH-HANS'
+      ]
+    
+    def getResult(self, source_text, target_lang):
+      result = self.client.translate(source_text, target_language=target_lang)
+      return result.get('translatedText')
 
 # 30 langs, 100 lines
-def getMachineTranslation():
+def getDeepLTranslation():
   deepl = DeepL()
 
   english_gt_path = "floresp-v2.0-rc.3/devtest/devtest.eng_Latn"
@@ -65,5 +103,22 @@ def getMachineTranslation():
         file.write(str(deepl.getResult(line, lang)))
     print("wrote to ", lang)
 
+# 30 langs, 100 lines
+def getGoogleTranslation():
+  google = GoogleTranslate()
+  english_gt_path = "floresp-v2.0-rc.3/devtest/devtest.eng_Latn"
+  english_gt = []
+  with open(english_gt_path, 'r') as file:
+    english_gt = file.readlines()
+
+  english_gt = english_gt[:100]
+  print(len(english_gt))
+
+  for lang in google.target_langs:
+    with open(f'closed_source_eval/googletranslate/{lang}.txt', 'w') as file:
+      for line in english_gt:
+        file.write(str(google.getResult(line, lang)))
+    print("wrote to ", lang)
+
 if __name__ == "__main__":
-  getMachineTranslation()
+  getGoogleTranslation()
